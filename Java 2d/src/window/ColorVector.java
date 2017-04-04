8fcb122ba818b7f086d370bf4a09b29688cbee26
unouser <unouser@PKI278-NUC28.unomaha.edu>
2017-04-04 17:14:18 -0500

    color

---
+++ Java 2d/src/window/ColorVector.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package window;
+
+import java.awt.Color;
+import java.awt.Point;
+
+/**
+ *
+ * @author unouser
+ */
+public class ColorVector {
+    public Point point;
+    public Color color;
+    
+    public ColorVector(Point point, Color color) {
+        this.point = point;
+        this.color = color;
+    }
+}
